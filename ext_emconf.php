a2d32ff16c24792fee2ffc703bfedbd15108aa5d
jsfan <jsfan@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-15 04:18:24 +0000

    Merge branch 'master' of file:///home/christian/TYPO3/git/cl_alphabeticalindex
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cl_alphabeticalindex/trunk@54171 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "cl_alphabeticalindex"
+# 
+# Auto generated 23-06-2006 10:30
+# 
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'Alphabetical site index',
+	'description' => 'Displays an alphabetical index of all pages of the site',
+	'category' => 'plugin',
+	'author' => 'Christian Lerrahn',
+	'author_email' => 'typo3@penpal4u.net',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'private' => '',
+	'download_password' => '',
+	'version' => '0.1.4',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"9119";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4af0";s:14:"ext_tables.php";s:4:"a986";s:13:"locallang.php";s:4:"9727";s:13:"locallang.xml";s:4:"34db";s:16:"locallang_db.php";s:4:"ea2d";s:14:"doc/manual.sxw";s:4:"c39d";s:19:"doc/wizard_form.dat";s:4:"71f4";s:20:"doc/wizard_form.html";s:4:"ff20";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:40:"pi1/class.tx_clalphabeticalindex_pi1.php";s:4:"7ca3";s:48:"pi1/class.tx_clalphabeticalindex_pi1_wizicon.php";s:4:"bd28";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.php";s:4:"4815";s:17:"pi1/locallang.xml";s:4:"78e1";s:20:"static/editorcfg.txt";s:4:"397a";s:16:"static/setup.txt";s:4:"fa60";}',
+);
+
+?>
---
+++ locallang_db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Language labels for database tables/fields belonging to extension "cl_alphabeticalindex"
+ *
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'tt_content.list_type_pi1' => 'Alphabetical site index',	
+	),
+);
+?>
